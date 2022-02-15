45f12e2b96237d40317022b2427e2cd7f252b847
Pavlo <pmishchenko-ua@memsql.com>
2022-02-15 15:30:32 +0200

    Add Managed Service to CI
    Summary: We want to start using S2MS in our CircleCI tests.
    
    Test Plan: https://app.circleci.com/pipelines/github/memsql/dbt-singlestore/24/workflows/77886fa4-0281-42e4-9d5d-eecd6645c7c4
    
    Reviewers: noprysk-ua, amakarovych-ua, hniemchenko-ua, okramarenko-ua, posadcha-ua
    
    Reviewed By: noprysk-ua
    
    Subscribers: engineering-list
    
    JIRA Issues: PLAT-6023, PLAT-6057
    
    Differential Revision: https://grizzly.internal.memcompute.com/D54629

---
+++ .circleci/s2ms_cluster.py
@@ -0,0 +1,135 @@
+import json
+import os
+import pymysql
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+import sys
+from time import sleep
+from typing import Dict, Optional
+
+BASE_URL = "https://api.singlestore.com"
+CLUSTERS_PATH = "/v0beta/clusters"
+
+SQL_USER_PASSWORD = os.getenv("SQL_USER_PASSWORD")  # project UI env-var reference
+S2MS_API_KEY = os.getenv("S2MS_API_KEY")  # project UI env-var reference
+
+HEADERS = {
+    "Authorization": f"Bearer {S2MS_API_KEY}",
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
+
+CLUSTER_NAME = "dbt-connector-ci-test-cluster"
+AWS_EU_CENTRAL_REGION = "7e7ffd27-20f7-44b6-87e6-e72828a81ac7"
+AUTO_TERMINATE_MINUTES = 20
+
+PAYLOAD_FOR_CREATE = {
+    "name": CLUSTER_NAME,
+    "regionID": AWS_EU_CENTRAL_REGION,
+    "adminPassword": SQL_USER_PASSWORD,
+    "expiresAt": f"{AUTO_TERMINATE_MINUTES}m",
+    "firewallRanges": [
+        "0.0.0.0/0"
+    ],
+    "size": "S-00"
+}
+HOSTNAME_TMPL = "svc-{}-ddl.aws-frankfurt-1.svc.singlestore.com"
+CLUSTER_ID_FILE = "CLUSTER_ID"
+
+TOTAL_RETRIES = 5
+S2MS_REQUEST_TIMEOUT = 60
+
+
+def request_with_retry(request_method, url, data=None, headers=HEADERS):
+    try:
+        with requests.Session() as s:
+            retries = Retry(
+                total=TOTAL_RETRIES,
+                backoff_factor=0.2,
+                status_forcelist=[500, 502, 503, 504])
+
+            s.mount('http://', HTTPAdapter(max_retries=retries))
+            s.mount('https://', HTTPAdapter(max_retries=retries))
+
+            return s.request(request_method, url, data=data, headers=headers, timeout=S2MS_REQUEST_TIMEOUT)
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
+
+def create_cluster() -> str:
+    cl_id = request_with_retry("POST", BASE_URL + CLUSTERS_PATH, data=json.dumps(PAYLOAD_FOR_CREATE))
+    return cl_id.json()["clusterID"]
+
+
+def get_cluster_info(cluster_id: str) -> Dict:
+    cl_id = request_with_retry("GET", BASE_URL + CLUSTERS_PATH + f"/{cluster_id}")
+    return cl_id.json()
+
+
+def is_cluster_active(cluster_id: str) -> bool:
+    cl_info = get_cluster_info(cluster_id)
+    return cl_info["state"] == "Active"
+
+
+def wait_start(cluster_id: str) -> None:
+    print(f"Waiting for cluster {cluster_id} to be available for connection..", end="", flush=True)
+    time_wait = 0
+    while (not is_cluster_active(cluster_id) and time_wait < 600):
+        print(".", end="", flush=True)
+        sleep(5)
+        time_wait += 5
+    if time_wait < 600:
+        print("\nCluster is active!")
+    else:
+        print(f"\nTimeout error: can't connect to {cluster_id} for more than 10 minutes!")
+
+
+def terminate_cluster(cluster_id: str) -> None:
+    print(f"Terminating cluster {cluster_id}...")
+    request_with_retry("DELETE", BASE_URL + CLUSTERS_PATH + f"/{cluster_id}")
+
+
+def check_connection(cluster_id: str, create_db: Optional[str] = None):
+    conn = pymysql.connect(
+        user="admin",
+        password=SQL_USER_PASSWORD,
+        host=HOSTNAME_TMPL.format(cluster_id),
+        port=3306)
+
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT NOW():>TEXT")
+        res = cur.fetchall()
+        print(f"Successfully connected to {cluster_id} at {res[0][0]}")
+
+        if create_db is not None:
+            cur.execute(f"DROP DATABASE IF EXISTS {create_db}")
+            cur.execute(f"CREATE DATABASE {create_db}")
+    finally:
+        cur.close()
+        conn.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Not enough arguments to start/terminate cluster!")
+        exit(1)
+    command = sys.argv[1]
+    db_name = None
+    if len(sys.argv) > 2:
+        db_name = sys.argv[2]
+
+    if command == "start":
+        new_cl_id = create_cluster()
+        with open(CLUSTER_ID_FILE, "w") as f:
+            f.write(new_cl_id)
+        wait_start(new_cl_id)
+        check_connection(new_cl_id, db_name)
+        exit(0)
+
+    if command == "terminate":
+        with open(CLUSTER_ID_FILE, "r") as f:
+            cl_id = f.read()
+        terminate_cluster(cl_id)
+        exit(0)
--- .circleci/setup-cluster.sh
+++ .circleci/setup-cluster.sh
@@ -1,60 +1,91 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-DEFAULT_IMAGE_NAME="singlestore/cluster-in-a-box:centos-7.6.5-018454f4e3-4.0.1-1.13.0"
-IMAGE_NAME="${SINGLESTORE_IMAGE:-$DEFAULT_IMAGE_NAME}"
-CONTAINER_NAME="singlestore-integration"
-EXTERNAL_MASTER_PORT=3306
-EXTERNAL_LEAF_PORT=3307
+start-s2ms-cluster() {
+  python ./.circleci/s2ms_cluster.py start dbt_test
+  export S2_HOST=svc-$(cat CLUSTER_ID)-ddl.aws-frankfurt-1.svc.singlestore.com
+  export S2_PORT=3306
+  export S2_USER=admin
+}
 
+terminate-s2ms-cluster() {
+  python ./.circleci/s2ms_cluster.py terminate
+}
 
-EXISTS=$(docker inspect ${CONTAINER_NAME} >/dev/null 2>&1 && echo 1 || echo 0)
+start-cluster-in-a-box() {
+  DEFAULT_IMAGE_NAME="singlestore/cluster-in-a-box:centos-7.5.12-3112a491c2-4.0.0-1.12.5"
+  IMAGE_NAME="${SINGLESTORE_IMAGE:-$DEFAULT_IMAGE_NAME}"
+  CONTAINER_NAME="singlestore-integration"
+  EXTERNAL_MASTER_PORT=3306
+  EXTERNAL_LEAF_PORT=3307
 
-if [[ "${EXISTS}" -eq 1 ]]; then
-  EXISTING_IMAGE_NAME=$(docker inspect -f '{{.Config.Image}}' ${CONTAINER_NAME})
-  if [[ "${IMAGE_NAME}" != "${EXISTING_IMAGE_NAME}" ]]; then
-    echo "Existing container ${CONTAINER_NAME} has image ${EXISTING_IMAGE_NAME} when ${IMAGE_NAME} is expected; recreating container."
-    docker rm -f ${CONTAINER_NAME}
-    EXISTS=0
+  EXISTS=$(docker inspect ${CONTAINER_NAME} >/dev/null 2>&1 && echo 1 || echo 0)
+
+  if [[ "${EXISTS}" -eq 1 ]]; then
+    EXISTING_IMAGE_NAME=$(docker inspect -f '{{.Config.Image}}' ${CONTAINER_NAME})
+    if [[ "${IMAGE_NAME}" != "${EXISTING_IMAGE_NAME}" ]]; then
+      echo "Existing container ${CONTAINER_NAME} has image ${EXISTING_IMAGE_NAME} when ${IMAGE_NAME} is expected; recreating container."
+      docker rm -f ${CONTAINER_NAME}
+      EXISTS=0
+    fi
   fi
-fi
 
-if [[ "${EXISTS}" -eq 0 ]]; then
+  if [[ "${EXISTS}" -eq 0 ]]; then
     docker run -i --init \
-        --name ${CONTAINER_NAME} \
-        -e LICENSE_KEY=${LICENSE_KEY} \
-        -e ROOT_PASSWORD=${ROOT_PASSWORD} \
-        -p $EXTERNAL_MASTER_PORT:3306 -p $EXTERNAL_LEAF_PORT:3307 \
-        ${IMAGE_NAME}
-fi
+          --name ${CONTAINER_NAME} \
+          -e LICENSE_KEY=${LICENSE_KEY} \
+          -e ROOT_PASSWORD=${SQL_USER_PASSWORD} \
+          -p $EXTERNAL_MASTER_PORT:3306 -p $EXTERNAL_LEAF_PORT:3307 \
+          ${IMAGE_NAME}
+  fi
 
-docker start ${CONTAINER_NAME}
+  docker start ${CONTAINER_NAME}
 
-singlestore-wait-start() {
-  echo -n "Waiting for SingleStore to start..."
-  while true; do
-      if mysql -u root -h 127.0.0.1 -P $EXTERNAL_MASTER_PORT -p"${ROOT_PASSWORD}" -e "select 1" >/dev/null 2>/dev/null; then
+  singlestore-wait-start() {
+    echo -n "Waiting for SingleStore to start..."
+    while true; do
+      if mysql -u root -h 127.0.0.1 -P $EXTERNAL_MASTER_PORT -p"${SQL_USER_PASSWORD}" -e "select 1" >/dev/null 2>/dev/null; then
           break
       fi
       echo -n "."
       sleep 0.2
-  done
-  echo ". Success!"
+    done
+    echo ". Success!"
+  }
+
+  singlestore-wait-start
+
+  echo
+  echo "Ensuring child nodes are connected using container IP"
+  CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_NAME})
+  CURRENT_LEAF_IP=$(mysql -u root -h 127.0.0.1 -P 3306 -p"${SQL_USER_PASSWORD}" --batch -N -e 'SELECT HOST FROM INFORMATION_SCHEMA.LEAVES')
+  if [[ ${CONTAINER_IP} != "${CURRENT_LEAF_IP}" ]]; then
+    # remove leaf with current ip
+    mysql -u root -h 127.0.0.1 -P $EXTERNAL_MASTER_PORT -p"${SQL_USER_PASSWORD}" --batch -N -e "REMOVE LEAF '${CURRENT_LEAF_IP}':3307"
+    # add leaf with correct ip
+    mysql -u root -h 127.0.0.1 -P $EXTERNAL_MASTER_PORT -p"${SQL_USER_PASSWORD}" --batch -N -e "ADD LEAF root:'${SQL_USER_PASSWORD}'@'${CONTAINER_IP}':3307"
+  fi
+
+  mysql -u root -h 127.0.0.1 -P $EXTERNAL_MASTER_PORT -p"${SQL_USER_PASSWORD}" --batch -N -e "DROP DATABASE IF EXISTS dbt_test; CREATE DATABASE dbt_test"
+
+  export S2_HOST=127.0.0.1
+  export S2_PORT=$EXTERNAL_MASTER_PORT
+  export S2_USER=root
+  echo "Done!"
 }
 
-singlestore-wait-start
 
-echo
-echo "Ensuring child nodes are connected using container IP"
-CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_NAME})
-CURRENT_LEAF_IP=$(mysql -u root -h 127.0.0.1 -P 3306 -p"${ROOT_PASSWORD}" --batch -N -e 'SELECT HOST FROM INFORMATION_SCHEMA.LEAVES')
-if [[ ${CONTAINER_IP} != "${CURRENT_LEAF_IP}" ]]; then
-    # remove leaf with current ip
-    mysql -u root -h 127.0.0.1 -P $EXTERNAL_MASTER_PORT -p"${ROOT_PASSWORD}" --batch -N -e "REMOVE LEAF '${CURRENT_LEAF_IP}':3307"
-    # add leaf with correct ip
-    mysql -u root -h 127.0.0.1 -P $EXTERNAL_MASTER_PORT -p"${ROOT_PASSWORD}" --batch -N -e "ADD LEAF root:'${ROOT_PASSWORD}'@'${CONTAINER_IP}':3307"
+if [ $1 == "start" ]
+then
+  if [ $2 == "ciab" ]
+  then
+    start-cluster-in-a-box
+  else
+    start-s2ms-cluster
+  fi
+else
+  if [ $2 == "s2ms" ]
+  then
+    terminate-s2ms-cluster
+  fi
 fi
-
-mysql -u root -h 127.0.0.1 -P $EXTERNAL_MASTER_PORT -p"${ROOT_PASSWORD}" --batch -N -e "DROP DATABASE IF EXISTS dbt_test; CREATE DATABASE dbt_test"
-
-echo "Done!"
