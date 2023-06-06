34c086bf6ad2c588d6727de4b3b3d385b4e18bd4
Olha Kramarenko <okramarenko-ua@singlestore.com>
2023-06-06 17:10:34 +0300

    Add tests introduced in dbt 1.3

--- dbt/include/singlestore/macros/utils/array_construct.sql
+++
@@ -1,7 +0,0 @@
-{% macro singlestore__array_construct(inputs, data_type) -%}
-    {% if inputs|length > 0 %}
-    [ {{ inputs|join(', ') }} ]
-    {% else %}
-    CAST(NULL AS ARRAY<{{ data_type }}>)
-    {% endif %}
-{%- endmacro %}
