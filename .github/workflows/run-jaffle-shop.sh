155ade52c9c19aaacc05660974a850669d03b418
Olha Kramarenko <okramarenko-ua@singlestore.com>
2025-08-05 17:24:13 +0300

    fix for S2MS

--- .github/workflows/run-jaffle-shop.sh
+++ .github/workflows/run-jaffle-shop.sh
@@ -4,6 +4,7 @@ source .env3/bin/activate
 source ./.github/workflows/setup-cluster.sh start $CLUSTER_TYPE $CIAB_IMAGE
 
 export S2_PASSWORD=$SQL_USER_PASSWORD  # project UI env-var reference
+WORKSPACE_ENDPOINT_FILE="WORKSPACE_ENDPOINT_FILE"
 
 create_dbs() {
   if [ "$CLUSTER_TYPE" = "ciab" ]; then
