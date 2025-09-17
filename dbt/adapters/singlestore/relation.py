600867ec2bc7df4abc310d8f137e2827bc44161e
Olha Kramarenko <okramarenko-ua@singlestore.com>
2025-09-17 16:53:38 +0300

    adding some fixes for test

--- dbt/adapters/singlestore/relation.py
+++ dbt/adapters/singlestore/relation.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 
-from dbt.adapters.base.relation import BaseRelation, Policy
+from dbt.adapters.base.relation import BaseRelation, Policy, EventTimeFilter
 from dbt_common.exceptions import DbtRuntimeError
 
 
@@ -31,3 +31,26 @@ class SingleStoreRelation(BaseRelation):
                 "but only one can be set"
             )
         return super().render()
+
+    def _norm(ts: str) -> str:
+        s = ts.replace('T', ' ').replace('Z', '')
+        s = re.sub(r'([+-]\d{2}:\d{2})$', '', s)   # drop timezone
+        return s[:19]                              # 'YYYY-MM-DD HH:MM:SS'
+    
+    def _render_event_time_filtered(self, event_time_filter: EventTimeFilter) -> str:
+        """
+        Returns "" if start and end are both None
+        """
+        filter = ""
+        if event_time_filter.start and event_time_filter.end:
+            filter = f"({event_time_filter.field_name} :> TIMESTAMP) >= '{_norm(event_time_filter.start)}' and ({event_time_filter.field_name} :> TIMESTAMP) < '{_norm(event_time_filter.end)}'"
+        elif event_time_filter.start:
+            filter = (
+                f"({event_time_filter.field_name} :> TIMESTAMP) >= '{_norm(event_time_filter.start)}'"
+            )
+        elif event_time_filter.end:
+            filter = (
+                f"({event_time_filter.field_name} :> TIMESTAMP) < '{_norm(event_time_filter.end)}'"
+            )
+
+        return filter
