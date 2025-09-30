b3f70ddaf426a8c22113558192928d9805dcb690
Olha Kramarenko <okramarenko-ua@singlestore.com>
2025-09-30 13:49:22 +0300

    Revert "fix basic snapshot tests + workarounds for valid_to"
    This reverts commit 92b517c914460c0b91939a33f746143033e2e202.

--- dbt/include/singlestore/macros/materializations/snapshot/snapshot_merge.sql
+++ dbt/include/singlestore/macros/materializations/snapshot/snapshot_merge.sql
@@ -1,10 +1,10 @@
 {% macro singlestore__snapshot_merge_sql_update(target, source, insert_cols) -%}
-    update {{ target }}, (select dbt_scd_id, dbt_change_type, ( dbt_valid_to :> datetime ) as dbt_valid_to from {{ source }}) as dbt_internal_source
+    update {{ target }}, (select dbt_scd_id, dbt_change_type, dbt_valid_to from {{ source }}) as dbt_internal_source
        set {{ target }}.dbt_valid_to = dbt_internal_source.dbt_valid_to
      where dbt_internal_source.dbt_scd_id = {{ target }}.dbt_scd_id
-       and dbt_internal_source.dbt_change_type in ('update','delete')
+       and dbt_internal_source.dbt_change_type = 'update'
       {% if config.get("dbt_valid_to_current") %}
-       and ( {{ target }}.dbt_valid_to = ( {{ config.get('dbt_valid_to_current') }} :> datetime ) or
+       and ( {{ target }}.dbt_valid_to = {{ config.get('dbt_valid_to_current') }} or
             {{ target }}.dbt_valid_to is null)
      {% else %}
        and {{ target }}.dbt_valid_to is null
