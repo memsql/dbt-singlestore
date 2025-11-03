5901bd893f13c0f404734be2805993f7d623eba2
Olha Kramarenko <okramarenko-ua@singlestore.com>
2025-11-03 18:24:21 +0200

    add test_dbt_clone

---
+++ tests/functional/adapter/test_dbt_clone.py
@@ -0,0 +1,19 @@
+import pytest
+
+from dbt.tests.adapter.dbt_clone.test_dbt_clone import (
+    BaseCloneNotPossible,
+    BaseClonePossible,
+    BaseCloneSameTargetAndState,
+)
+
+
+class TestBaseCloneNotPossible(BaseCloneNotPossible):
+    pass
+
+
+class TestBaseClonePossible(BaseClonePossible):
+    pass
+
+
+class TestCloneSameTargetAndState(BaseCloneSameTargetAndState):
+    pass
